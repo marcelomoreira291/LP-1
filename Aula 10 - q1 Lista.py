992e6b6f3299b2ecc42613269f30712a059154d2
Marcelo Moreira <marcelo.moreira@aluno.uece.br>
2021-03-28 19:16:22 -0300

    ainda fazendo raiva

---
+++ Aula 10 - q1 Lista.py
@@ -0,0 +1,7 @@
+'''
+1 - Implemente a classe Funcionário com os seguintes atributos
+(matrícula, setor, valor_hora, número de faltas),
+construa métodos que você acha útil para essa classe.
+'''
+
+
